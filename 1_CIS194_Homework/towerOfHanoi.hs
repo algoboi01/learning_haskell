3f7463cfed876fa6f37c216667187e28dfa8e2ef
Algoboi01 <sahilohe97@gmail.com>
2023-09-07 22:10:45 +0530

    TowerOfHanoi problem solution

--- 1_CIS194_Homework/towerOfHanoi.hs
+++ 1_CIS194_Homework/towerOfHanoi.hs
@@ -4,12 +4,18 @@
 
 module TowerOfHanoi where
 
+-- As per described in the problem
 type Peg = String
 type Move = (Peg, Peg)
 
--- returns a list of moves to be performed 
-hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi x fromPeg toPeg auxPeg 
-   | x < 0     = [] 
-   | otherwise = (fromPeg, auxPeg) : hanoi (x-1) auxPeg fromPeg toPeg
+-- Function Decleration 
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move] 
+hanoi n fromPeg toPeg auxPeg 
+   | n == 0      = [] -- stops when there are no more disks left (base case)
+   | otherwise = hanoi (n - 1) fromPeg auxPeg toPeg ++ [(fromPeg, toPeg)] ++ hanoi (n - 1) auxPeg toPeg fromPeg
+
+-- n       -> number of disks
+-- fromPeg -> peg from which one disk will be moved
+-- toPeg   -> peg to which the disk will be transfered
+-- auxPeg  -> auxillary peg to which disks can be transfered (also known as the helper peg)
 
