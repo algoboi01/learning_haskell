9b3deb67b1057ab2a69e537e85c7e3f573ce3147
Algoboi01 <sahilohe97@gmail.com>
2023-09-19 20:54:58 +0530

    made changes in the function

--- 1_CIS194_Homework/H2/LogAnalysis.hs
+++ 1_CIS194_Homework/H2/LogAnalysis.hs
@@ -7,27 +7,27 @@ import Log
 
 -- Excercise 1 Solution
 -- it works (the way it should i guess)
--- ignore the rule of meaningful argument names
+-- ignore the rule of meaningful argument & function names
 parseMessage :: String -> LogMessage
 parseMessage x
-    | s !! 0 == "E" = LogMessage (Error error_type) timestamp_r message_r
-    | s !! 0 == "I" = LogMessage Info timestamp message
-    | s !! 0 == "W" = LogMessage Warning timestamp message 
+    | head s == "E" = LogMessage (Error error_type) timestamp_r message_r
+    | head s == "I" = LogMessage Info timestamp message
+    | head s == "W" = LogMessage Warning timestamp message 
     | otherwise     = Unknown x
       where
          s = words x -- seperates the contents after a space
          -- as Error takes an argument, the seperated string will be different from other types of log message
          error_type  = read $ s !! 1
          timestamp_r = read $ s !! 2
-         message_r   = addString $ drop 3 s
+         message_r   = combineString $ drop 3 s
          -- for other Log message types (Warning, Info)
          timestamp   = read $ s !! 1
-         message     = addString $ drop 2 s
+         message     = combineString $ drop 2 s
 
 -- Creates a string from a list containing String elements
-addString :: [String] -> String
-addString [] = ""
-addString (x:xs) = x ++ " " ++ addString xs -- leaves a space at the end but anyways
+combineString :: [String] -> String
+combineString [] = ""
+combineString (x:xs) = x ++ " " ++ combineString xs -- leaves a space at the end but anyways
 
 -- parse :: String -> [LogMessage]
 
